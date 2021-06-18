60c1a90de1da7f553667954d3776cf43dd56f0f3
LishaRudani <77lisharudani@gmail.com>
2021-06-18 18:26:14 +0530

    YouTube API For YouTube Player

---
+++ app/src/main/java/com/example/youtubeapplication/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.youtubeapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends YouTubeBaseActivity {
+    public static final String API_KEY ="AIzaSyBvyLE62whM4DoDmBi-KtCaDmUmzAyznIE";
+
+    YouTubePlayerView youTubePlayerView;
+    YouTubePlayer.OnInitializedListener onInitializedListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        youTubePlayerView=findViewById(R.id.youtube2);
+        onInitializedListener=new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("LaFtAcIrGWA");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+        youTubePlayerView.initialize(API_KEY,onInitializedListener);
+    }
+}
